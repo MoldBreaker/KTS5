73e4063a06367b1044dfe0999c8ad849fbe6bdce
MoldBreaker <vunguyen10a8.nt@gmail.com>
2025-03-27 10:10:08 +0700

    update edit.php

--- app/views/edit.php
+++ app/views/edit.php
@@ -1,21 +1,44 @@
-<form method="POST">
-    <label>Mã nhân viên:</label>
-    <input type="text" name="Ma_NV" value="<?= $nhanVien['Ma_NV'] ?>" readonly>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thêm nhân viên</title>
+    <link rel="stylesheet" href="/KTS5/public/css/add.css">
+</head>
+<body>
+    <div class="container">
+        <h2>SuaSua Nhân Viên</h2>
+        <?php if (isset($_SESSION['error'])) { ?>
+            <p style="color:red; text-align: center;"> <?php echo $_SESSION['error']; unset($_SESSION['error']); ?> </p>
+        <?php } ?>
 
-    <label>Tên nhân viên:</label>
-    <input type="text" name="Ten_NV" value="<?= $nhanVien['Ten_NV'] ?>" required>
+        <form method="POST">
+            <label for="Ma_NV">Mã Nhân Viên:</label>
+            <input type="text" name="Ma_NV" value="<?= $nhanVien['Ma_NV'] ?>" readonly>
 
-    <label>Phái:</label>
-    <input type="text" name="Phai" value="<?= $nhanVien['Phai'] ?>" required>
+            <label for="Ten_NV">Tên Nhân Viên:</label>
+            <input type="text" name="Ten_NV" value="<?= $nhanVien['Ten_NV'] ?>" required>
 
-    <label>Nơi sinh:</label>
-    <input type="text" name="Noi_Sinh" value="<?= $nhanVien['Noi_Sinh'] ?>" required>
+            <label for="Phai">Giới Tính:</label>
+            <select name="Phai" value="<?= $nhanVien['Phai'] ?>">
+                <option value="NAM">Nam</option>
+                <option value="NU">Nữ</option>
+            </select>
 
-    <label>Mã phòng:</label>
-    <input type="text" name="Ma_Phong" value="<?= $nhanVien['Ma_Phong'] ?>" required>
+            <label for="Noi_Sinh">Nơi Sinh:</label>
+            <input type="text" value="<?= $nhanVien['Noi_Sinh'] ?>" name="Noi_Sinh" required>
 
-    <label>Lương:</label>
-    <input type="number" name="Luong" value="<?= $nhanVien['Luong'] ?>" required>
+            <label for="Noi_Sinh">Ma Phong:</label>
+            <input type="text" name="Ma_Phong" value="<?= $nhanVien['Ma_Phong'] ?>" required>
 
-    <button type="submit">Cập nhật</button>
-</form>
+            <label for="Luong">Lương:</label>
+            <input type="number" name="Luong" value="<?= $nhanVien['Luong'] ?>" required>
+
+            <button type="submit">Edit Nhân Viên</button>
+        </form>
+
+        <a href="/KTS5" class="back-link">Quay lại danh sách</a>
+    </div>
+</body>
+</html>
